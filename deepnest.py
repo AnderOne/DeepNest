454f2b9811c7405196e874d7b124ee62bba05607
Ander <armath123@gmail.com>
2019-05-13 21:50:45 +1000

    Minor fixes

--- deepnest.py
+++ deepnest.py
@@ -5,19 +5,27 @@ class DeepIterator:
 
 	def __next__(self):
 
-		while (self.top) and (self.top[-1][1] >= len(self.top[-1][0])): self.top.pop()
+		while self.top and self.top[-1][1] >= len(self.top[-1][0]):
+			self.top.pop()
 
-		if not self.top: raise StopIteration()
+		if not self.top:
+			raise StopIteration()
 
 		buf = self.top[-1]
 		if len(buf) == 3:
-			key = buf[2][buf[1]]; obj = buf[0][key]; self.obj = (key, obj)
+			key = buf[2][buf[1]]
+			obj = buf[0][key]
+			self.obj = (key, obj)
 		else:
-			pos = buf[1]; obj = buf[0][pos]; self.obj = obj
+			pos = buf[1]
+			obj = buf[0][pos]
+			self.obj = obj
 		buf[1] += 1
 
 		if   type(obj) in (frozenset, set, dict):
-			self.top.append([obj, 0, sorted(obj.keys())])
+			self.top.append(
+			[obj, 0, sorted(obj.keys())]
+			)
 		elif type(obj) in (tuple, list):
 			self.top.append([obj, 0])
 		else:
@@ -37,17 +45,17 @@ class DeepWrapper:
 
 	def __eq__(self, rhs):
 
-		if type(rhs) is not DeepWrapper: raise TypeError()
-		lhs = DeepIterator(self.obj)
-		rhs = DeepIterator(rhs.obj)
+		if type(rhs) is not DeepWrapper:
+			raise TypeError()
+
+		lhs = DeepIterator(self.obj); rhs = DeepIterator(rhs.obj)
 		for itl, itr in zip(lhs, rhs):
 			if lhs.level() != rhs.level(): return False
 			if type(itl) is not type(itr): return False
 			if type(itl) in (
-				str, float, int, bool, None
+			str, float, int, bool, None
 			):
-				if itl == itr: continue
-				return False
+				if itl != itr: return False
 
 		try: next(lhs)
 		except StopIteration: enl = True
