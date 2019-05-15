ea7d9d587c1382fdf5bdfc30eb78dad77509af26
Ander <armath123@gmail.com>
2019-05-15 11:11:20 +1000

    Update example

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+import json
 from deepnest import *
 
 def print_dfs(obj):
@@ -36,6 +37,67 @@ def test4():
 	print(a == b)
 	print(a != b)
 
+def test5():
+
+	def check(a):
+		s = dumps(a)
+		b = loads(s)
+		print(
+			a, ' -> ', s, ' -> ', b
+		)
+
+	check("abcxyz")
+	check("\\\"")
+	check(-9.05)
+	check(-1)
+	check(False)
+	check(True)
+	check(None)
+	check(())
+	check([])
+	check({})
+
+def test6():
+	a = {
+		'a': False,
+		'b': True,
+		'c': [
+			1,
+			{
+				'd': "abc+@#!",
+				'e': [2, 3, 4]
+			},
+			5
+		],
+		'f': {
+			'g': -6.078,
+			'i': 9
+		},
+		'j': None,
+		'k': 0
+	}
+	print(a)
+	s = dumps(a)
+	print(s)
+	b = loads(s)
+	print(b)
+
+def test7():
+	a = []
+	for i in range(1000): a = [i, a]
+	try:
+		s = json.dumps(a)
+	except RecursionError:
+		print('NO:(')
+	s = dumps(a)
+	print('YES!')
+	try:
+		b = json.loads(s)
+	except RecursionError:
+		print('NO:(')
+	b = loads(s)
+	print('YES!')
+
 print('------------')
 test1()
 print('------------')
@@ -44,3 +106,9 @@ print('------------')
 test3()
 print('------------')
 test4()
+print('------------')
+test5()
+print('------------')
+test6()
+print('------------')
+test7()
